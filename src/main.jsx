ae96433655fed1f5245bfb1eb57c58646b1b9917
kazvee <kazveedev@gmail.com>
2025-03-09 15:03:17 -0600

    feat(App): integrate Navbar component into main layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './components/Navbar.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
+    <NavigationBar />
     <App />
-  </StrictMode>,
-)
+  </>
+);
