882f07d3068e95f7ab16b225673f97f754fa7f91
kazvee <kazveedev@gmail.com>
2025-03-19 16:50:58 -0600

    refactor(RestaurantList): use cards for restaurant details

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,19 +1,38 @@
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import restaurants from '../data/restaurants.json';
 
 function RestaurantList() {
   return (
-    <div>
-      <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>
-            <p>{restaurant.name}</p>
-            <p>{restaurant.cuisine}</p>
-            <p>{restaurant.postcode}</p>
-            <p>{restaurant.recommended.join(', ')}</p>
-          </li>
+    <Container className='mt-4'>
+      <Row xs={1} md={2} lg={3} className='g-4'>
+        {restaurants.map((restaurant, index) => (
+          <Col key={index}>
+            <Card className='h-100 border-double'>
+              <Card.Body>
+                <Card.Title>{restaurant.name}</Card.Title>
+                <Card.Subtitle className='mb-2'>
+                  {restaurant.cuisine}
+                </Card.Subtitle>
+                <Card.Text>
+                  <strong>Postcode:</strong> {restaurant.postcode}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Recommended:</strong>
+                </Card.Text>
+                <ul className='restaurant-list'>
+                  {restaurant.recommended.map((dish, index) => (
+                    <li key={index}>{dish}</li>
+                  ))}
+                </ul>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
-    </div>
+      </Row>
+    </Container>
   );
 }
 
