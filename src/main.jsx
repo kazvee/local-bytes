1ded035f38779ebb6a248e2b0ad6c240ab34e81e
kazvee <kazveedev@gmail.com>
2025-03-22 17:06:57 -0600

    refactor(main): remove unnecessary imports to simplify root render

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <>
-    <NavigationBar />
     <App />
-    <Footer />
   </>
 );
