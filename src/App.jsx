379a32c57ba49460fdc97202836205048dacde43
kazvee <109990289+kazvee@users.noreply.github.com>
2026-03-28 11:37:55 -0600

    feat(app): toggle between RestaurantList and About

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 import NavigationBar from './components/Navbar';
+import About from './components/About';
 import RestaurantList from './components/RestaurantList';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
+  const [showAbout, setShowAbout] = useState(false);
 
-  const handleSearch = (query) => {
-    setSearchQuery(query);
-  };
+  const handleSearch = (query) => setSearchQuery(query);
 
   return (
     <>
       <NavigationBar onSearch={handleSearch} />
-      <RestaurantList searchQuery={searchQuery} />
-      <Footer />
+      {showAbout ? <About /> : <RestaurantList searchQuery={searchQuery} />}
+      <Footer onAboutClick={() => setShowAbout(true)} />
     </>
   );
 }
